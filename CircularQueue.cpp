df0f6b2827605a59588f8317adf3d8a8a057bcb0
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-06-10 11:02:19 +0700

    Implement conditions to display functions

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -75,6 +75,21 @@
                 return;
             }
             cout << "\nElements in the queue are .. \n";
+
+            if (FRONT_position <= REAR_position){
+                while(FRONT_position <= REAR_position){
+                    cout<< queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+                cout<<endl;
+            }
+
+            else{
+                while (FRONT_position <= max -1 ){
+                    cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+            }
         }
 
 
