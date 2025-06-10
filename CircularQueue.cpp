cf2a84b2c8cd8596a2c3ebbab1f7069bfa86c752
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-06-10 10:39:02 +0700

    Insert function

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -11,4 +11,17 @@
             REAR = -1;
         }
 
-        
+        void insert (){
+            int num;
+            cout<<"Enter a number:";
+            cin >> num;
+            cout << endl;
+
+            //Cek apakah antrian penuh
+
+            if ((FRONT == 0 && REAR == max -1 ) || (FRONT == REAR + 1)){
+                cout << "\n Queue overflow\n";
+                return;
+            }
+        }
+ }
